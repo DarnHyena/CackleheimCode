e5bd6c7817e284aa4298c0ee7932be405db9a793
Jules <github@sirskunkalot.de>
2021-08-21 17:06:42 +0200

    import unity project files and rename mod stub

---
+++ Cackleheim/Cackleheim.cs
@@ -0,0 +1,29 @@
+﻿// Cackleheim
+// a Valheim mod using Jötunn
+// 
+// File:    Cackleheim.cs
+// Project: Cackleheim
+
+using BepInEx;
+using BepInEx.Configuration;
+using Jotunn.Utils;
+using UnityEngine;
+
+namespace Cackleheim
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    internal class Cackleheim : BaseUnityPlugin
+    {
+        public const string PluginGUID = "es.localhyena.Cackleheim";
+        public const string PluginName = "Cackleheim";
+        public const string PluginVersion = "0.0.1";
+
+        private void Awake()
+        {
+            // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
+            Jotunn.Logger.LogInfo("ModStub has landed");
+        }
+    }
+}
