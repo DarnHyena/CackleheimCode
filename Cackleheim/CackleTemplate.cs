2fe51cfb4b697f479882c125a21ebe46a26ab0c4
DarnHyena <60151350+DarnHyena@users.noreply.github.com>
2021-08-27 12:28:56 -0500

    Updated Template

--- Cackleheim/CackleTemplate.cs
+++ Cackleheim/CackleTemplate.cs
@@ -11,30 +11,32 @@ using Jotunn.Managers;
 using Jotunn.Utils;
 using UnityEngine;
 
-namespace Cackle-
-{
+namespace CackleNAME
+{   
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
     internal class Cackle- : BaseUnityPlugin
     {
-        public const string PluginGUID = "DarnHyena.Cackle-";
-        public const string PluginName = "Cackle-";
+        public const string PluginGUID = "DarnHyena.CackleNAME";
+        public const string PluginName = "CackleNAME";
         public const string PluginVersion = "0.0.1";
 
         private GameObject HatObj;
         private GameObject PantObj;
         private GameObject CapeObj;
+        //private GameObject ChestObj;
 
 
         private void Awake()
         {
-            //========ASSETBUNDLES========//
+            //========ASSETBUNDLES========// 
 
-            AssetBundle -Bundle = AssetUtils.LoadAssetBundleFromResources("item-", typeof(Cackle-).Assembly);
-            HatObj = -Bundle.LoadAsset<GameObject>("Item01");
-            PantObj = -Bundle.LoadAsset<GameObject>("Item02");
-            CapeObj = -Bundle.LoadAsset<GameObject>("Item03");
+            AssetBundle NAMEBundle = AssetUtils.LoadAssetBundleFromResources("itemNAME", typeof(CackleNAME).Assembly);
+            HatObj = NAMEBundle.LoadAsset<GameObject>("Item01");
+            PantObj = NAMEBundle.LoadAsset<GameObject>("Item02");
+            CapeObj = NAMEBundle.LoadAsset<GameObject>("Item03");
+         //   ChestObj = NAMEBundle.LoadAsset<GameObject>("Item04");
             -Bundle.Unload(false);
 
 
@@ -42,12 +44,13 @@ namespace Cackle-
 
             CustomItem hatItem = new CustomItem(HatObj, true, new ItemConfig()
             {
-                CraftingStation = "piece_workbench",
+                CraftingStation = "piece_workbench", //Note: These look for the name of the crafting table prefabs. 
+                MinStationLevel = 0,  //Level crafting table needs to be in order to make the item. Aka, those things you place around it
                 Requirements = new RequirementConfig[]
                 {
                     new RequirementConfig()
                     {
-                        Item = "===",
+                        Item = "RESOURCEHERE",
                         Amount = 0,
                         AmountPerLevel = 0
                     }
@@ -60,17 +63,18 @@ namespace Cackle-
             CustomItem capeItem = new CustomItem(CapeObj, true, new ItemConfig()
             {
                 CraftingStation = "piece_workbench",
+                MinStationLevel = 0,
                 Requirements = new RequirementConfig[]
                 {
                     new RequirementConfig()
                     {
-                        Item = "==",
+                        Item = "RESOURCEHERE",
                         Amount = 0,
                         AmountPerLevel = 0
                     },
                     new RequirementConfig()
                     {
-                        Item = "==",
+                        Item = "RESOURCEHERE",
                         Amount = 0,
                         AmountPerLevel = 0
                     }
@@ -83,11 +87,12 @@ namespace Cackle-
             CustomItem pantItem = new CustomItem(PantObj, true, new ItemConfig()
             {
                 CraftingStation = "piece_workbench",
+                MinStationLevel = 0,
                 Requirements = new RequirementConfig[]
     {
                     new RequirementConfig()
                     {
-                        Item = "==",
+                        Item = "RESOURCEHERE",
                         Amount = 0,
                         AmountPerLevel = 0
                     }
@@ -95,13 +100,33 @@ namespace Cackle-
             });
             ItemManager.Instance.AddItem(pantItem);
 
+            //============================//
+            //Remove the /* */ on either side of this section below to turn it back on//
 
-            //===Item Names, Description===//
-            //========&Localization========//
-
-            LocalizationManager.Instance.AddLocalization(new LocalizationConfig("English")
+         /*   CustomItem chestItem = new CustomItem(ChestObj, true, new ItemConfig()
             {
-                Translations = {
+                CraftingStation = "piece_workbench",
+                MinStationLevel = 0,
+                Requirements = new RequirementConfig[]
+    {
+                    new RequirementConfig()
+                    {
+                        Item = "RESOURCEHERE",
+                        Amount = 0,
+                        AmountPerLevel = 0
+                    }
+                }
+            });
+            ItemManager.Instance.AddItem(chestItem); */
+
+        //============================//
+
+        //===Item Names, Description===//
+        //========&Localization========//
+
+        LocalizationManager.Instance.AddLocalization(new LocalizationConfig("English")
+            {
+                Translations = { //Don't include the $ for the item id, the code doesn't like those
                     {"ID01", "Name01" },
                     {"IDDesc01", "Desc01"},
                     {"ID02", "Name02" },
